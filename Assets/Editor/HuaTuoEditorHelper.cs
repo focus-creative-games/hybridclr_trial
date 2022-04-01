5ac0d0650edc12115c320d216d0e7aa4ffbc2506
walon <taojingjian@gmail.com>
2022-04-01 17:50:06 +0800

    更新测试代码。

--- Assets/Editor/HuaTuoEditorHelper.cs
+++ Assets/Editor/HuaTuoEditorHelper.cs
@@ -61,6 +61,51 @@ namespace HuaTuo
             AssetDatabase.CopyAsset("Assets/HuaTuo/Output/huatuo", "Assets/StreamingAssets/huatuo");
         }
 
+        [MenuItem("HuaTuo/Build/BuildSceneBundle", false, 1)]
+        public static void BuildSeneAssetBundle()
+        {
+            string _dllPath = Application.dataPath + "/Scenes/HotUpdate.unity";
+            string _tarDir = Application.dataPath + "/HuaTuo/Temp/";
+            string _tarPath = $"{_tarDir}HotUpdate.bytes";
+            string _outPutPath = Application.dataPath + "/HuaTuo/Output/";
+
+            if (!Directory.Exists(_tarDir))
+            {
+                Directory.CreateDirectory(_tarDir);
+            }
+
+            if (!Directory.Exists(_outPutPath))
+            {
+                Directory.CreateDirectory(_outPutPath);
+            }
+
+            if (File.Exists(_tarPath))
+            {
+                File.Delete(_tarPath);
+            }
+
+            FileStreamCopy(_dllPath, _tarPath);
+
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+
+            string _assetPath = _tarPath.Substring(_tarPath.IndexOf("Assets/", StringComparison.Ordinal));
+
+            List<AssetBundleBuild> _list = new List<AssetBundleBuild>();
+            AssetBundleBuild _ab = new AssetBundleBuild
+            {
+                assetBundleName = "huatuo",
+                assetNames = new[] { _assetPath }
+            };
+            _list.Add(_ab);
+
+            BuildPipeline.BuildAssetBundles(_outPutPath, _list.ToArray(), BuildAssetBundleOptions.None,
+                EditorUserBuildSettings.activeBuildTarget);
+
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+
+            AssetDatabase.CopyAsset("Assets/HuaTuo/Output/huatuo", "Assets/StreamingAssets/huatuo");
+        }
+
         public static void FileStreamCopy(string _src, string _tar)
         {
             using (FileStream _read = new FileStream(_src, FileMode.OpenOrCreate, FileAccess.Read))
--- Assets/HotFix/App.cs
+++ Assets/HotFix/App.cs
@@ -7,42 +7,7 @@ public class App
 {
     public static int Main()
     {
-        Debug.Log("hello,huatuo ab");
-
-        TestAsync();
-
-        Debug.Log("hello,huatuo 2");
-        return 0;
-    }
-
-    public static async void TestAsync()
-    {
-        Debug.Log("async task 1");
-        await Task.Delay(10);
-        Debug.Log("async task 2");
-    }
-
-    public static int Main_1()
-    {
         Debug.Log("hello,huatuo");
-
-        var task = Task.Run(async () =>
-        {
-            await TestAsync2();
-        });
-
-        task.Wait();
-
-        Debug.Log("async task end");
-        Debug.Log("async task end2");
-
         return 0;
-    }
-
-    public static async Task TestAsync2()
-    {
-        Debug.Log("async task 1");
-        await Task.Delay(3000);
-        Debug.Log("async task 2");
     }
 }
