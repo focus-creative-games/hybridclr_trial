14af4f7512aef13ff0381e4da1a24bb09269ddcf
walon <taojingjian@gmail.com>
2022-04-26 21:19:31 +0800

    将项目文件移出版本管理

--- copydll.bat
+++
@@ -1,5 +0,0 @@
-copy Library\ScriptAssemblies\HotFix.dll Debug\huatuo_Data\StreamingAssets\HotFix.dll 
-copy Library\ScriptAssemblies\HotFix.dll Build-Win64\build\bin\huatuo_Data\StreamingAssets\HotFix.dll 
-copy Library\ScriptAssemblies\HotFix.dll Assets\StreamingAssets\HotFix.dll 
-
-pause
