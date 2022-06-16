b6ccb77f4beed671f15fd5e87121d9cbf6aa6814
walon <taojingjian@gmail.com>
2022-06-16 20:39:05 +0800

    除去 .bat中的中文字符，因为.git 拉下来后，换行符会被转为\n unix格式，运行出错

--- HuatuoData/init_huatuo_repos.bat
+++ HuatuoData/init_huatuo_repos.bat
@@ -1,5 +1,4 @@
 
-rem clone huatuo仓库,国内推荐用 gitee
 rem git clone https://github.com/focus-creative-games/huatuo
 git clone https://gitee.com/focus-creative-games/huatuo huatuo_repo
 
@@ -7,7 +6,7 @@ rem git clone https://github.com/pirunxi/il2cpp_huatuo
 git clone https://gitee.com/juvenior/il2cpp_huatuo il2cpp_huatuo_repo
 
 
-rem 设置默认分支为2020.3.33，避免很多人忘了切分支
+rem set default branch
 set DEFAULT_VERSION=2020.3.33
 cd il2cpp_huatuo_repo
 
--- HuatuoData/init_local_il2cpp_data.bat
+++ HuatuoData/init_local_il2cpp_data.bat
@@ -1,12 +1,10 @@
 
 
-rem 请修改为你所用Unity的il2cpp目录
-rem 一般像这样 C:\Program Files\Unity\Hub\Editor\2020.3.33f1c2\Editor\Data\il2cpp
-
+rem replace with right Unity Editor Install path
 set IL2CPP_PATH=C:\Program Files\Unity\Hub\Editor\2020.3.33f1c2\Editor\Data\il2cpp
 
 if not exist "%IL2CPP_PATH%" (
-    echo "你未指定正确的il2cpp路径"
+    echo "please set correct %IL2CPP_PATH% value"
     goto EXIT
 )
 
@@ -16,37 +14,29 @@ if not exist %LOCAL_IL2CPP_DATA% (
     mkdir %LOCAL_IL2CPP_DATA%
 )
 
-
-rem Unity 打包时允许使用环境变量UNITY_IL2CPP_PATH自定义%IL2CPP_PATH%的位置
-rem 但同时又要求同级目录包含MonoBleedingEdge，因此需要拷贝这两个目录
-
-rem 拷贝 MonoBleedingEdge 目录
+rem need copdy MonoBleedingEdge
 set MBE=%LOCAL_IL2CPP_DATA%\MonoBleedingEdge
 if not exist %MBE% (
     xcopy /q /i /e "%IL2CPP_PATH%\..\MonoBleedingEdge" %MBE%
 )
 
 
-rem 拷贝il2cpp目录
+rem copy il2cpp
 set IL2CPP=%LOCAL_IL2CPP_DATA%\il2cpp
 if not exist %IL2CPP% (
     xcopy /q /i /e "%IL2CPP_PATH%" %IL2CPP%
 )
 
-rem 接下来替换 il2cpp目录下的libil2cpp为 huatuo修改后的版本
-rem 需要使用 {https://gitee.com/juvenior/il2cpp_huatuo}/libil2cpp 替换 il2cpp/libil2cpp目录
-rem 需要使用 {https://gitee.com/focus-creative-games/huatuo}/huatuo 添加到 il2cpp/libil2cpp目录下，即il2cpp/libil2cpp/huatuo
-
 set HUATUO_REPO=huatuo_repo
 
 if not exist %HUATUO_REPO% (
-    echo 未安装huatuo https://gitee.com/focus-creative-games/huatuo,请运行 init_huatuo_repos.bat or init_huatuo_repos.sh
+    echo not install huatuo https://gitee.com/focus-creative-games/huatuo, please run init_huatuo_repos.bat or init_huatuo_repos.sh
     goto EXIT 
 )
 
 set IL2CPP_HUATUO_REPO=il2cpp_huatuo_repo
 if not exist %IL2CPP_HUATUO_REPO% (
-    echo 未安装il2cpp_huatuo https://gitee.com/juvenior/il2cpp_huatuo ,请运行 init_huatuo_repos.bat or init_huatuo_repos.sh
+    echo not install il2cpp_huatuo https://gitee.com/juvenior/il2cpp_huatuo , please run init_huatuo_repos.bat or init_huatuo_repos.sh
     goto EXIT 
 )
 
@@ -56,12 +46,12 @@ rd /s /q %LIBIL2CPP_PATH%
 xcopy /q /i /e %IL2CPP_HUATUO_REPO%\libil2cpp %LIBIL2CPP_PATH%
 xcopy /q /i /e %HUATUO_REPO%\huatuo %LIBIL2CPP_PATH%\huatuo
 
-rem 务必清除缓存，不然build仍然使用旧版本。
-rem 只影响直接build的情况，不影响导出工程的情形。
-echo 清除 Library\Il2cppBuildCache 缓存目录
-rd /s /q ..\Library\Il2cppBuildCache
+rem clean il2cpp build cache
+set IL2CPP_CACHE=..\Library\Il2cppBuildCache
+echo clean %IL2CPP_CACHE%
+if exist "%IL2CPP_CACHE%" rd /s /q "%IL2CPP_CACHE%"
 
-echo 初始化成功
+echo succ
 
 :EXIT
 
