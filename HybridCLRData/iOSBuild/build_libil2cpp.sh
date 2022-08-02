b6c89af8819a68302c1cd081f0cc6ea93ce7ae87
kkvskkkk <lanzhihong@pohope.com>
2022-08-02 15:19:55 +0800

    更新打包脚本， 支持自动设置ios编译环境，支持更丰富的选项

--- HybridCLRData/iOSBuild/build_libil2cpp.sh
+++ HybridCLRData/iOSBuild/build_libil2cpp.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 export HUATUO_IL2CPP_SOURCE_DIR=$(pushd ../LocalIl2CppData/il2cpp > /dev/null && pwd && popd > /dev/null)
-export IPHONESIMULATOR_VERSION=
 
 rm -rf build
 
 mkdir build
 cd build
-cmake ..
-make -j4
 
-if [ -f "libil2cpp.a" ]
+# 设置Bitcode -DENABLE_BIECODE_INT=1
+# 模拟器平台 -DPLATFORM=SIMULATOR64
+# 设置部署版本 -DDEPLOYMENT_TARGET=10.10
+# 不设置CMAKE_TOOLCHAIN_FILE 导出的mac版本 不过CMakeLists.txt没有兼容mac版本
+
+cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../CMake/ios.toolchain.cmake -DPLATFORM=OS64 -DHUATUO_IL2CPP_SOURCE_DIR=${HUATUO_IL2CPP_SOURCE_DIR} -DOUTPUT_BIN_DIR=lib
+
+xcodebuild -project il2cpp.xcodeproj -alltargets -configuration Release
+
+if [ -f "lib/libil2cpp.a" ]
 then
 	echo 'build succ'
 else
