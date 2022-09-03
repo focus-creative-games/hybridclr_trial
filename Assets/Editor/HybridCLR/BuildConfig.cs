8ad8172f5543793f8b6f6d7f1db9696a4683311e
walon <walon@focus-creative-games.com>
2022-09-03 18:28:53 +0800

    [fix] 修复2019版本iOS没有设置IL2CPP_PATH的问题（2020起iOS由于独立build，不需要设置）

--- Assets/Editor/HybridCLR/BuildConfig.cs
+++ Assets/Editor/HybridCLR/BuildConfig.cs
@@ -11,7 +11,7 @@ namespace HybridCLR.Editor
 {
     public static partial class BuildConfig
     {
-#if !UNITY_IOS
+#if !UNITY_2020_1_OR_NEWER || !UNITY_IOS
         [InitializeOnLoadMethod]
         private static void Setup()
         {
