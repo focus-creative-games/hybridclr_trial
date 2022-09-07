6a57cbc52c7dc07ae6e5ab08f20a96271c555202
walon <walon@focus-creative-games.com>
2022-09-07 20:47:40 +0800

    [fix] 修复 SetUpOnLoad时调用 SettingsUtil中ScriptableObject相关操作，而此时资源未完成初始化，导致xxxSettings失效的bug

--- Packages/com.focus-creative-games.hybridclr_unity/Editor/BuildProcessors/CheckSettings.cs
+++ Packages/com.focus-creative-games.hybridclr_unity/Editor/BuildProcessors/CheckSettings.cs
@@ -17,12 +17,15 @@ namespace HybridCLR.Editor.BuildProcessors
         {
             if (!SettingsUtil.Enable)
             {
+
+#if !UNITY_2020_1_OR_NEWER || !UNITY_IOS
                 string oldIl2cppPath = Environment.GetEnvironmentVariable("UNITY_IL2CPP_PATH");
                 if (!string.IsNullOrEmpty(oldIl2cppPath))
                 {
                     Environment.SetEnvironmentVariable("UNITY_IL2CPP_PATH", "");
                     Debug.Log($"[BPCheckSettings] 清除 UNITY_IL2CPP_PATH, 旧值为:'{oldIl2cppPath}'");
                 }
+#endif
                 return;
             }
             if (UnityEditor.PlayerSettings.gcIncremental)
@@ -30,12 +33,16 @@ namespace HybridCLR.Editor.BuildProcessors
                 Debug.LogError($"[BPCheckSettings] HybridCLR不支持增量式GC，已经自动将该选项关闭");
                 UnityEditor.PlayerSettings.gcIncremental = false;
             }
+
+
+#if !UNITY_2020_1_OR_NEWER || !UNITY_IOS
             string curIl2cppPath = Environment.GetEnvironmentVariable("UNITY_IL2CPP_PATH");
             if (curIl2cppPath != SettingsUtil.LocalIl2CppDir)
             {
                 Environment.SetEnvironmentVariable("UNITY_IL2CPP_PATH", SettingsUtil.LocalIl2CppDir);
                 Debug.Log($"[BPCheckSettings] UNITY_IL2CPP_PATH 当前值为:'{curIl2cppPath}'，更新为:'{SettingsUtil.LocalIl2CppDir}'");
             }
+#endif
         }
     }
 }
--- Packages/com.focus-creative-games.hybridclr_unity/Editor/SetUpOnLoad.cs
+++ Packages/com.focus-creative-games.hybridclr_unity/Editor/SetUpOnLoad.cs
@@ -11,7 +11,7 @@ namespace HybridCLR.Editor
     internal static class SetUpOnLoad
     {
 #if !UNITY_2020_1_OR_NEWER || !UNITY_IOS
-        [InitializeOnLoadMethod]
+        //[InitializeOnLoadMethod]
         private static void Setup()
         {
             if (!SettingsUtil.Enable)
@@ -23,26 +23,26 @@ namespace HybridCLR.Editor
             /// unity允许使用UNITY_IL2CPP_PATH环境变量指定il2cpp的位置，因此我们不再直接修改安装位置的il2cpp，
             /// 而是在本地目录
             ///
-            var installerController = new Installer.InstallerController();
-            if (!installerController.HasInstalledHybridCLR())
-            {
-                if (installerController.CheckValidIl2CppInstallDirectory(installerController.Il2CppBranch, installerController.Il2CppInstallDirectory) == Installer.InstallErrorCode.Ok)
-                {
-                    if(EditorUtility.DisplayDialog(
-                          "初始化HybridCLR", // title
-                          "检测到未初始化HybridCLR，是否自动安装", // description
-                          "Yes",
-                          "No"
-                        ))
-                    {
-                        installerController.InitHybridCLR(installerController.Il2CppBranch, installerController.Il2CppInstallDirectory);
-                    }
-                }
-                if (!installerController.HasInstalledHybridCLR())
-                {
-                    Debug.LogError($"未初始化HybridCLR,请在菜单 HybridCLR/Installer 中执行安装");
-                }
-            }
+            //var installerController = new Installer.InstallerController();
+            //if (!installerController.HasInstalledHybridCLR())
+            //{
+            //    if (installerController.CheckValidIl2CppInstallDirectory(installerController.Il2CppBranch, installerController.Il2CppInstallDirectory) == Installer.InstallErrorCode.Ok)
+            //    {
+            //        if(EditorUtility.DisplayDialog(
+            //              "初始化HybridCLR", // title
+            //              "检测到未初始化HybridCLR，是否自动安装", // description
+            //              "Yes",
+            //              "No"
+            //            ))
+            //        {
+            //            installerController.InitHybridCLR(installerController.Il2CppBranch, installerController.Il2CppInstallDirectory);
+            //        }
+            //    }
+            //    if (!installerController.HasInstalledHybridCLR())
+            //    {
+            //        Debug.LogError($"未初始化HybridCLR,请在菜单 HybridCLR/Installer 中执行安装");
+            //    }
+            //}
         }
 #endif
     }
