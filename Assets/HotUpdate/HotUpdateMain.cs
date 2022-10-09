d4a3e78cec49b4a20338eabe57a1e24d30f18c7a
walon <walon@focus-creative-games.com>
2022-10-09 09:52:23 +0800

    [refactor] HotFix -> HotUpdate，同时修复一些名字错误

--- Assets/HotUpdate/HotUpdateMain.cs
+++ Assets/HotUpdate/HotUpdateMain.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HotifxMain : MonoBehaviour
+public class HotUpdateMain : MonoBehaviour
 {
 
     public string text;
